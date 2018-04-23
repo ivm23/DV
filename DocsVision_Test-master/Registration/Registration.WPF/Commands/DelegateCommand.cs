b58e6dfc8ae66820badb214a86f6d52142ae83c5
Ivanova Marina <marina200964@yandex.ru>
2018-04-23 18:53:05 +0300

    test with wpf

---
+++ DocsVision_Test-master/Registration/Registration.WPF/Commands/DelegateCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Registration.WPF.Commands
+{
+    public class DelegateCommand : ICommand
+    {
+        Func<object, bool> _canExecute;
+        Action<object> _execute;
+
+        //Конструктор
+        public DelegateCommand(Func<object, bool> canExecute, Action<object> execute)
+        {
+            this._canExecute = canExecute;
+            this._execute = execute;
+        }
+
+        //Проверка доступности команды
+        public bool CanExecute(object parameter)
+        {
+            return this._canExecute(parameter);
+        }
+
+        //Выполнение команды
+        public void Execute(object parameter)
+        {
+            this._execute(parameter);
+        }
+
+        //Служебное событие
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested += value; }
+        }
+    }
+}
---
+++ DocsVision_Test-master/Registration/Registration.WPF/ViewModels/AuthorizationViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.ComponentModel;
+
+namespace Registration.WPF.ViewModels
+{
+    class AuthorizationViewModel : INotifyPropertyChanged
+    {
+        public string Name
+        {
+            get { return this._name; }
+            set
+            {
+                this._name = value;
+
+                OnPropertyChanged("Name");
+                OnPropertyChanged("HelloText");
+            }
+        }
+
+        public string HelloText
+        {
+            get
+            {
+                return "fff";//_service.SayHello(this.Name);
+            }
+        }
+
+        string _name;
+
+        public AuthorizationViewModel()
+        {
+            //    this._service = service;
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this,
+                        new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
+
